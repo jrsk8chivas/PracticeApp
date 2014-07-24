921d889e30b345e135723c93f067f83286284169
jrsk8chivas <jr_chivas123@yahoo.com>
2014-07-23 17:35:13 -0700

    Initial commit got a log in screen

---
+++ app/src/main/java/com/newthinkank/helloworld/app/MyActivity.java
@@ -0,0 +1,52 @@
+package com.newthinkank.helloworld.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MyActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        final TextView firstTextView = (TextView) findViewById(R.id.textView);
+
+        final Button firstButton = (Button) findViewById(R.id.firstButton);
+
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                firstTextView.setHighlightColor(48);
+                firstTextView.setText("Welcome");
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/newthinkank/helloworld/app/User.java
@@ -0,0 +1,19 @@
+package com.newthinkank.helloworld.app;
+
+/**
+ * Created by jose.herrera on 7/23/2014.
+ */
+public class User {
+
+        public long userId;
+        public String username;
+        public String password;
+
+        public User(long userId, String username, String password){
+            this.userId=userId;
+            this.username=username;
+            this.password=password;
+        }
+
+    }
+
