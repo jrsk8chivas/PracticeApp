3b0778624d936317fe2cbad16f0487d8a7cd65e1
jrsk8chivas <jr_chivas123@yahoo.com>
2014-07-31 23:29:37 -0700

    Add menu

--- app/src/main/java/com/newthinkank/helloworld/app/MyActivity.java
+++ app/src/main/java/com/newthinkank/helloworld/app/MyActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 
 public class MyActivity extends ActionBarActivity {
+
+    private final int ID_MENU_EXIT =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +27,42 @@ public class MyActivity extends ActionBarActivity {
             @Override
             public void onClick(View view) {
 
-                firstTextView.setHighlightColor(48);
                 firstTextView.setText("Welcome");
             }
         });
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
+
+        // call the base class method
+        super.onCreateOptionsMenu(menu);
+
+        //the menu option text is defined in resources
+        menu.add(R.string.aboutOption);
+
+        //get a SubMenu reference
+        SubMenu sm = menu.addSubMenu("Options...");
+        //add menu items to the submenu
+        sm.add("Theme");
+        sm.add("Settings");
+
+        //it is better to use final variables for IDs than constant values
+        //menu.add(Menu.NONE,1,Menu.NONE,"Exit");
+
+        //get the MenuItem reference
+        MenuItem item =
+                menu.add(Menu.NONE,ID_MENU_EXIT,Menu.NONE,R.string.exitOption);
+        //set the shortcut
+        item.setShortcut('5', 'x');
+
+        //the menu option text is defined as constant String
+        menu.add("Restart");
+
         return true;
+
     }
 
     @Override
